d9d46d611d313933127d95244f52712f511c7015
Jan Czok <jan.czok@outlook.de>
2021-12-15 11:14:15 +0100

    connected html and js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /*
 Axios frägt Websiten ab und liefert die Rohdaten
 */
-const axios = require('axios');
-
 // Make a request for a user with a given ID
 axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   .then(function (response) {
     // handle success
+
+    document.getElementById("title").innerHTML = response.data.title;
+
     console.log(response.data.hdurl);
+    console.log(response.data.title);
     console.log(response.data.explanation);
     console.log(response.data.copyright); 
     console.log(response.data.date);
 });
-
-// TODO: Daten visualisieren
